edac304f9041e1f9d83c01a3cd56070509268808
Konrad Sochacki <sochacki.konrad@gmail.com>
2020-10-20 12:28:32 +0200

    Rename summarizing_and_topic_modelling

--- summarizing_and_topic_modelling_demo/lda_algorithmia.py
+++ summarizing_and_topic_modelling_demo/lda_algorithmia.py
@@ -3,7 +3,7 @@ import Algorithmia
 from text_format_util import get_text_from_wikipedia, read_book_and_format
 
 # raw_text, formatted_text = get_text_from_wikipedia("https://en.wikipedia.org/wiki/Tsavo_Man-Eaters")
-raw_text, formatted_text = read_book_and_format("egyptian-history.txt")
+raw_text, formatted_text = read_book_and_format("summarizing_and_topic_modelling/egyptian-history.txt")
 
 input = {
     "docsList": [
--- summarizing_and_topic_modelling_demo/main.py
+++ summarizing_and_topic_modelling_demo/main.py
@@ -5,12 +5,16 @@ import bs4 as bs
 import nltk
 import heapq
 
+from rouge_score import rouge_scorer
+
 from nlp_processing import find_weighted_frequencies, calculate_sentences_score
 from text_format_util import get_text_from_wikipedia, read_book_and_format
 
 
 def get_tsavo_man_eaters_wiki_summary():
     raw_text, formatted_text = get_text_from_wikipedia("https://en.wikipedia.org/wiki/Tsavo_Man-Eaters")
+
+    print("Tsavo text:\n", raw_text)
 
     # Converting Text To Sentences
     sentence_list = nltk.sent_tokenize(raw_text)
@@ -25,7 +29,7 @@ def get_tsavo_man_eaters_wiki_summary():
     summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
 
     summary = ' '.join(summary_sentences)
-    return summary
+    return summary, raw_text, formatted_text
 
 
 def get_egyptian_history_book_summary():
@@ -44,11 +48,17 @@ def get_egyptian_history_book_summary():
     summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
 
     summary = ' '.join(summary_sentences)
-    return summary
+    return summary, raw_text, formatted_text
 
 
-# summary = get_tsavo_man_eaters_wiki_summary()
-# summary = get_egyptian_history_book_summary()
-# print(summary)
+tsavo_summary, tsavo_raw_text, tsavo_formatted_text = get_tsavo_man_eaters_wiki_summary()
+# print("Summary:\n", tsavo_summary)
+egypt_summary, egypt_raw_text, egypt_formatted_text = get_egyptian_history_book_summary()
 
-test_gensim()
+scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
+tsavo_scores = scorer.score(tsavo_raw_text, tsavo_summary)
+egypt_scores = scorer.score(egypt_raw_text, egypt_summary)
+
+print("tsavo scores:\n", tsavo_scores)
+print("egypt scores:\n", egypt_scores)
+# test_gensim()
